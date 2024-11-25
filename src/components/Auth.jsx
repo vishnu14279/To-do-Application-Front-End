4b5e1e84fe426c220ee1ca793ee28abc406ead10
VISHNU PRIYAN M <80460869+vishnu14279@users.noreply.github.com>
2024-11-25 12:08:27 +0530

    Update Auth.jsx
    prod url changed

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -9,7 +9,7 @@ const { Option } = Select;
 const Auth = ({ setAuth }) => {
   const [isLogin, setIsLogin] = useState(true);
   const [form, setForm] = useState({ email: "", password: "", role: "User" });
-  const apiUrl = "http://localhost:3001";
+  const apiUrl = "https://to-do-application-back-end.onrender.com";
 
   const toggleMode = () => setIsLogin((prev) => !prev);
 
